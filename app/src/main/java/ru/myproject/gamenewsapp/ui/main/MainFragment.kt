ddf4503f40572611eaab76f10de0dedeaea76099
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-05-24 17:05:39 +0700

    add bindingDelegate

--- app/src/main/java/ru/myproject/gamenewsapp/ui/main/MainFragment.kt
+++ app/src/main/java/ru/myproject/gamenewsapp/ui/main/MainFragment.kt
@@ -1,32 +1,22 @@
 package ru.myproject.gamenewsapp.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.myproject.gameNewsAPP.R
 import ru.myproject.gameNewsAPP.databinding.FragmentMainBinding
+import ru.myproject.gamenewsapp.ui.base.base.viewBinding
 
-class MainFragment : Fragment() {
-  private var _binding: FragmentMainBinding? = null
-  private val binding get() = _binding!!
+class MainFragment : Fragment(R.layout.fragment_main) {
+  private val binding by viewBinding { FragmentMainBinding.bind(it) }
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    _binding=FragmentMainBinding.inflate(layoutInflater, container, false)
-    return binding.root
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.rvRecycleView
   }
 
-  override fun onViewStateRestored(savedInstanceState: Bundle?) {
-    super.onViewStateRestored(savedInstanceState)
-  }
 
-  override fun onDestroyView() {
-    super.onDestroyView()
-    _binding = null
-  }
+
 }
 
