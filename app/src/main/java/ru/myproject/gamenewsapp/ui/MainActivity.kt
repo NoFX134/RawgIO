2b10b9fd23bf1ab9da370ce5b903ce9fa9f0539e
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-05-24 15:14:06 +0700

    add first fragment

--- app/src/main/java/ru/myproject/gamenewsapp/ui/MainActivity.kt
+++ app/src/main/java/ru/myproject/gamenewsapp/ui/MainActivity.kt
@@ -3,11 +3,20 @@ package ru.myproject.gamenewsapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.myproject.gameNewsAPP.R
+import ru.myproject.gameNewsAPP.databinding.ActivityMainBinding
+import ru.myproject.gamenewsapp.ui.main.MainFragment
 
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding= ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    if(savedInstanceState==null){
+      supportFragmentManager.beginTransaction()
+        .add(R.id.container, MainFragment())
+        .commitAllowingStateLoss()
+    }
   }
 }
--- app/src/main/java/ru/myproject/gamenewsapp/ui/main/MainFragment.kt
+++ app/src/main/java/ru/myproject/gamenewsapp/ui/main/MainFragment.kt
@@ -5,18 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.myproject.gameNewsAPP.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+  private var _binding: FragmentMainBinding? = null
+  private val binding get() = _binding!!
+
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return super.onCreateView(inflater, container, savedInstanceState)
+    _binding=FragmentMainBinding.inflate(layoutInflater, container, false)
+    return binding.root
   }
 
   override fun onViewStateRestored(savedInstanceState: Bundle?) {
     super.onViewStateRestored(savedInstanceState)
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
   }
 }
 
